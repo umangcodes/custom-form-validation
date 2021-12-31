276a33822c36e3eb7dd7e7ab43b14e4364f431b2
Umang A <umanga.eng@gmail.com>
2021-12-30 20:53:58 -0500

    added mdi/js to the project

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import "./index.css";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import './assets/tailwind.css'
+import "./assets/tailwind.css";
+// import * as mdijs from "@mdi/js";
 
 createApp(App).use(store).use(router).mount("#app");
