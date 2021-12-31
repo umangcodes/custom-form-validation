e4a1350e6d00bade1383d5b4f847bed7b2279e64
Umang A <umanga.eng@gmail.com>
2021-12-30 20:55:02 -0500

    home page layout changed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="h-auto">
-    <Login />
+  <div class="h-auto flex text-2xl justify-center m-5">
+    <router-link :to="{ name: 'Login' }" class="mx-10">Login</router-link>
+    <router-link :to="{ name: 'Register' }" class="mx-10">Register</router-link>
+    <router-link :to="{ name: 'ResetPassword' }" class="mx-10"
+      >Reset</router-link
+    >
   </div>
 </template>
 
 <script>
-import Login from "../components/login/Login.vue";
+// import Login from "../components/login/Login.vue";
 // import Register from "../components/login/Register.vue";
 // import ResetPassword from "../components/login/ResetPassword.vue";
 
 export default {
   name: "Home",
   components: {
-    Login,
+    // Login,
     // Register,
     // ResetPassword,
   },
